7e116d9741d1b6e4e28ff8e5278b89e0b8f07376
Enrique Santos <santosnrique2121@gmail.com>
2022-10-12 19:57:41 -0300

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { TextField } from "./components/TextField";
+
+function App() {
+  return (
+    <div className="App">
+      <TextField>
+        <h1>Design System</h1>
+      </TextField>
+    </div>
+  );
+}
+
+export default App;
